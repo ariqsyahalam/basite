542c364f5eb4f80d52fde999836a4f008190608a
Reyhan Ariq Syahalam <ariqsyahalam@risya.id>
2023-01-22 13:12:06 +0700

    second

--- account-card.js
+++ account-card.js
@@ -12,26 +12,34 @@ class AccountCard extends HTMLElement {
     this.accountName = this.getAttribute("accountName");
     this.accountNumber = this.getAttribute("accountNumber");
     this.alias = this.getAttribute("alias");
+    this.image = this.getAttribute("image");
 
     this.render();
   }
 
   render() {
     this.innerHTML = `
-      <div class="container-sm p-3">
-          <div class="card">
-              <div class="card-body">
-                  <div>
-                      <h5 class="card-title">${this.accountName}</h5>
-                      <p> <b>${this.accountNumber}</b> a.n. ${this.alias}</p>
-                  </div>
-                  <div class="d-grid gap-2">
-                      <button onclick="myFunction('${this.accountNumber}')" onmouseout="outFunc()" class="btn btn-outline-dark">
-                          Copy Number
-                      </button>
-                  </div>
+      <div class="card mb-3 container-sm p-3" >
+        <div class="row g-0">
+          <div class="col-md-1 p-1 d-flex align-items-center">
+            <img src="${this.image}" class="img-fluid rounded-start" style="max-width:64px" >
+          </div>
+          <div class="col-md-1 p-1">
+          </div>
+          <div class="col-md-10">
+            <div class="card-body">
+                <div class="m-auto">
+                    <h5 class="card-title">${this.accountName}</h5>
+                    <p> <b>${this.accountNumber}</b> a.n. ${this.alias}</p>
+                </div>
+                <div class="d-grid gap-2">
+                <button onclick="myFunction('${this.accountNumber}')" onmouseout="outFunc()" class="btn btn-outline-dark">
+                    Copy Number
+                </button>
+                </div>
               </div>
           </div>
+        </div>
       </div>
       `;
   }
